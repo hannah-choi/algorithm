6f6a7884740b7a506d665cb3be0189ac3c265f8b
hannah-developer <hannahchoi.developer@gmail.com>
2022-12-06 07:55:02 +0000

    217 Contains Duplicate

---
+++ solutions/SlidingWindow/217_ContainsDuplicate.js
@@ -0,0 +1,12 @@
+// Problem: https://leetcode.com/problems/contains-duplicate/
+
+// O(n) Solution
+export const containsDuplicate = (nums) => {
+  const obj = {}; // Faster than using Set()
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (obj[num]) return true;
+    else obj[num] = 1;
+  }
+  return false;
+};
---
+++ solutions/SlidingWindow/217_ContainsDuplicate.test.js
@@ -0,0 +1,11 @@
+import { containsDuplicate } from "./217_ContainsDuplicate";
+
+describe.each([
+  [[1, 2, 3, 1], true],
+  [[1, 2, 3, 4], false],
+  [[1, 1, 1, 3, 3, 4, 3, 2, 4, 2], true],
+])(``, (arr, expected) => {
+  it(`${arr} returns ${expected}`, () => {
+    expect(containsDuplicate(arr)).toBe(expected);
+  });
+});
