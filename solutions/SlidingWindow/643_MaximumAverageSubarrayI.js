02212efdb8f756152ba3261017185c021f9b23fe
hannah-developer <hannahchoi.developer@gmail.com>
2022-12-06 09:24:44 +0000

    643 Maximum Average Subarray I

---
+++ solutions/SlidingWindow/643_MaximumAverageSubarrayI.js
@@ -0,0 +1,18 @@
+// Problem: https://leetcode.com/problems/maximum-average-subarray-i/
+
+// O(n) Solution
+export const findMaxAverage = (nums, k) => {
+  if (nums.length === 1) return nums[0];
+  let currentSum = 0;
+  let maxSum = -Infinity;
+
+  for (let i = 0; i < nums.length; i++) {
+    currentSum = currentSum + nums[i];
+    if (i >= k - 1) {
+      maxSum = Math.max(currentSum, maxSum);
+      currentSum -= nums[i - k + 1];
+    }
+  }
+
+  return maxSum / k;
+};
